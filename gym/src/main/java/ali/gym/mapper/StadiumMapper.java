6aea69b6432103c7f17dfd4ab5c329dacef6f229
heikexiaozi <wqpwqp1222@email.swu.edu.cn>
2025-04-05 16:35:53 +0800

    Delete gym directory

--- gym/src/main/java/ali/gym/mapper/StadiumMapper.java
+++
@@ -1,9 +0,0 @@
-package ali.gym.mapper;
-
-import ali.gym.entity.Stadium;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface StadiumMapper extends BaseMapper<Stadium> {
-}
--- gym/src/main/java/ali/gym/service/ShowService.java
+++
@@ -1,9 +0,0 @@
-package ali.gym.service;
-
-import ali.gym.entity.StatusInfo;
-
-import java.util.List;
-
-public interface ShowService {
-          public   List<StatusInfo> show(Integer id,int day);
-}
